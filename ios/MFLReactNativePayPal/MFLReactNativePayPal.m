7a278ccd36fb62a1582d9c681907b2dfcf1a3a72
Bernardo Avancena <ardowzrdowz@gmail.com>
2016-02-08 16:02:58 -0800

    modified simple pay to future payment

--- ios/MFLReactNativePayPal/MFLReactNativePayPal.m
+++ ios/MFLReactNativePayPal/MFLReactNativePayPal.m
@@ -13,9 +13,8 @@
 NSString * const kPayPalPaymentStatusKey              = @"status";
 NSString * const kPayPalPaymentConfirmationKey        = @"confirmation";
 
-@interface MFLReactNativePayPal () <PayPalPaymentDelegate, RCTBridgeModule>
+@interface MFLReactNativePayPal () <PayPalFuturePaymentDelegate, RCTBridgeModule>
 
-@property PayPalPayment *payment;
 @property PayPalConfiguration *configuration;
 @property (copy) RCTResponseSenderBlock flowCompletedCallback;
 
@@ -28,88 +27,126 @@ RCT_EXPORT_MODULE();
 RCT_EXPORT_METHOD(initializePaypalEnvironment:(int)environment
                   forClientId:(NSString *)clientId )
 {
-  dispatch_async(dispatch_get_main_queue(), ^{
-    NSString *envString = [self stringFromEnvironmentEnum:environment];
-
-    [PayPalMobile initializeWithClientIdsForEnvironments:@{envString : clientId}];
-    [PayPalMobile preconnectWithEnvironment:envString];
-  });
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSString *envString = [self stringFromEnvironmentEnum:environment];
+        
+        [PayPalMobile initializeWithClientIdsForEnvironments:@{envString : clientId}];
+        [PayPalMobile preconnectWithEnvironment:envString];
+    });
 }
 
 #pragma mark React Exported Methods
 
-RCT_EXPORT_METHOD(preparePaymentOfAmount:(NSString *)amount
-                  ofCurrency:(NSString *)currencyCode
-                  withDescription:(NSString *)description)
-{
-  self.payment = [[PayPalPayment alloc] init];
-  [self.payment setAmount:[[NSDecimalNumber alloc] initWithString:amount]];
-  [self.payment setCurrencyCode:currencyCode];
-  [self.payment setShortDescription:description];
-}
-
-
 RCT_EXPORT_METHOD(prepareConfigurationForMerchant:(NSString *)merchantName
-                  acceptingCreditCards:(BOOL)shouldAcceptCreditCards
-                  withUserEmail:(NSString *)userEmail)
+                  merchantPrivacyPolicy:(NSString *)privacyPolicy
+                  merchantUserAgreement:(NSString *)userAgreement)
 {
-  self.configuration = [[PayPalConfiguration alloc] init];
-  [self.configuration setMerchantName:merchantName];
-  [self.configuration setAcceptCreditCards:shouldAcceptCreditCards];
-  [self.configuration setDefaultUserEmail:userEmail];
+    self.configuration = [[PayPalConfiguration alloc] init];
+    [self.configuration setMerchantName:merchantName];
+    [self.configuration setMerchantPrivacyPolicyURL:[NSURL URLWithString:privacyPolicy]];
+    [self.configuration setMerchantUserAgreementURL:[NSURL URLWithString:userAgreement]];
 }
+
 
 RCT_EXPORT_METHOD(presentPaymentViewControllerForPreparedPurchase:(RCTResponseSenderBlock)flowCompletedCallback)
 {
-  self.flowCompletedCallback = flowCompletedCallback;
-
-  PayPalPaymentViewController *vc = [[PayPalPaymentViewController alloc] initWithPayment:self.payment
-                                                                           configuration:self.configuration
-                                                                                delegate:self];
-
-  UIViewController *visibleVC = [[[UIApplication sharedApplication] keyWindow] rootViewController];
-  do {
-    if ([visibleVC isKindOfClass:[UINavigationController class]]) {
-      visibleVC = [(UINavigationController *)visibleVC visibleViewController];
-    } else if (visibleVC.presentedViewController) {
-      visibleVC = visibleVC.presentedViewController;
-    }
-  } while (visibleVC.presentedViewController);
-
-  [visibleVC presentViewController:vc animated:YES completion:nil];
+    self.flowCompletedCallback = flowCompletedCallback;
+    
+    //  PayPalPaymentViewController *vc = [[PayPalPaymentViewController alloc] initWithPayment:self.payment
+    //                                                                           configuration:self.configuration
+    //                                                                                delegate:self];
+    PayPalFuturePaymentViewController *fpViewController;
+    fpViewController = [[PayPalFuturePaymentViewController alloc] initWithConfiguration:self.configuration
+                                                                               delegate:self];
+    
+    // Present the PayPalFuturePaymentViewController
+    UIViewController *visibleVC = [[[UIApplication sharedApplication] keyWindow] rootViewController];
+    do {
+        if ([visibleVC isKindOfClass:[UINavigationController class]]) {
+            visibleVC = [(UINavigationController *)visibleVC visibleViewController];
+        } else if (visibleVC.presentedViewController) {
+            visibleVC = visibleVC.presentedViewController;
+        }
+    } while (visibleVC.presentedViewController);
+    
+    [visibleVC presentViewController:fpViewController animated:YES completion:nil];
+    
+    //  UIViewController *visibleVC = [[[UIApplication sharedApplication] keyWindow] rootViewController];
+    //  do {
+    //    if ([visibleVC isKindOfClass:[UINavigationController class]]) {
+    //      visibleVC = [(UINavigationController *)visibleVC visibleViewController];
+    //    } else if (visibleVC.presentedViewController) {
+    //      visibleVC = visibleVC.presentedViewController;
+    //    }
+    //  } while (visibleVC.presentedViewController);
+    //
+    //  [visibleVC presentViewController:vc animated:YES completion:nil];
 }
 
-#pragma mark Paypal Delegate
+#pragma mark - PayPalFuturePaymentDelegate methods
 
-- (void)payPalPaymentDidCancel:(PayPalPaymentViewController *)paymentViewController
-{
-  [paymentViewController.presentingViewController dismissViewControllerAnimated:YES completion:^{
-    if (self.flowCompletedCallback) {
-      self.flowCompletedCallback(@[[NSNull null], @{kPayPalPaymentStatusKey : @(kPayPalPaymentCanceled)}]);
-    }
-  }];
+- (void)payPalFuturePaymentDidCancel:(PayPalFuturePaymentViewController *)futurePaymentViewController {
+    // User cancelled login. Dismiss the PayPalLoginViewController, breathe deeply.
+    //    [self dismissViewControllerAnimated:YES completion:nil];
+    [futurePaymentViewController.presentingViewController dismissViewControllerAnimated:YES completion:^{
+        if (self.flowCompletedCallback) {
+            self.flowCompletedCallback(@[[NSNull null], @{kPayPalPaymentStatusKey : @(kPayPalPaymentCanceled)}]);
+        }
+    }];
+    
 }
 
-- (void)payPalPaymentViewController:(PayPalPaymentViewController *)paymentViewController
-                 didCompletePayment:(PayPalPayment *)completedPayment
-{
-  [paymentViewController.presentingViewController dismissViewControllerAnimated:YES completion:^{
+- (void)payPalFuturePaymentViewController:(PayPalFuturePaymentViewController *)futurePaymentViewController
+                didAuthorizeFuturePayment:(NSDictionary *)futurePaymentAuthorization {
+    // The user has successfully logged into PayPal, and has consented to future payments.
+    
+    // Your code must now send the authorization response to your server.
+    
+    [futurePaymentViewController.presentingViewController dismissViewControllerAnimated:YES completion:^{
+        if (self.flowCompletedCallback) {
+            [self sendAuthorizationToServer:futurePaymentAuthorization];
+        }
+    }];
+    
+    
+    // Be sure to dismiss the PayPalLoginViewController.
+    //    [self dismissViewControllerAnimated:YES completion:nil];
+    [futurePaymentViewController.presentingViewController dismissViewControllerAnimated:YES completion:^{
+        if (self.flowCompletedCallback) {
+            self.flowCompletedCallback(@[[NSNull null], @{kPayPalPaymentStatusKey : @(kPayPalPaymentCanceled)}]);
+        }
+    }];
+}
+
+- (void)sendAuthorizationToServer:(NSDictionary *)authorization {
+    // Send the entire authorization reponse
+    NSData *consentJSONData = [NSJSONSerialization dataWithJSONObject:authorization
+                                                              options:0
+                                                                error:nil];
+    
+    NSString * myString = [[NSString alloc] initWithData:consentJSONData encoding:NSUTF8StringEncoding];
+    
     if (self.flowCompletedCallback) {
-      self.flowCompletedCallback(@[[NSNull null], @{kPayPalPaymentStatusKey : @(kPayPalPaymentCompleted),
-                                                    kPayPalPaymentConfirmationKey : completedPayment.confirmation}]);
+        self.flowCompletedCallback(@[myString, @{kPayPalPaymentStatusKey : @(kPayPalPaymentCanceled)}]);
     }
-  }];
+    // (Your network code here!)
+    //
+    // Send the authorization response to your server, where it can exchange the authorization code
+    // for OAuth access and refresh tokens.
+    //
+    // Your server must then store these tokens, so that your server code can execute payments
+    // for this user in the future.
 }
 
 #pragma mark Utilities
 
 - (NSString *)stringFromEnvironmentEnum:(PayPalEnvironment)env
 {
-  switch (env) {
-    case kPayPalEnvironmentProduction: return PayPalEnvironmentProduction;
-    case kPayPalEnvironmentSandbox: return PayPalEnvironmentSandbox;
-    case kPayPalEnvironmentSandboxNoNetwork: return PayPalEnvironmentNoNetwork;
-  }
+    switch (env) {
+        case kPayPalEnvironmentProduction: return PayPalEnvironmentProduction;
+        case kPayPalEnvironmentSandbox: return PayPalEnvironmentSandbox;
+        case kPayPalEnvironmentSandboxNoNetwork: return PayPalEnvironmentNoNetwork;
+    }
 }
 
 @end
